aa401bd4ffba20cf659896b07ac33c4f455d3968
StevenRCE0 <StevenRCE0@users.noreply.github.com>
2021-10-16 19:26:56 +0800

    Add script tools

---
+++ Script Toolbox/Nano.sh
@@ -0,0 +1,5 @@
+if [ -e ~/Wheelchair/WheelchairTumbler/main.py ]; then
+    python3 ~/Wheelchair/WheelchairTumbler/main.py
+else
+    echo 'main programme does not exist.'
+fi
---
+++ Script Toolbox/Run.sh
@@ -0,0 +1,2 @@
+scp -r .. root@192.168.1.133:~/Wheelchair
+ssh root@192.168.1.133 '~/Wheelchair/WheelchairTumbler/Script\ Toolbox/Nano.py'
